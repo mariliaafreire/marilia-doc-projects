b424e22a9fe1439773b50801fb0acd52c1c49c3e
marilia.freire@gmail.com <marilia.freire@gmail.com@fad937e2-7c9b-ea7b-840c-a1296998497c>
2011-02-10 18:56:24 +0000

    Processo simplificado para testes iniciais da abordagem composta com métricas... (dez 2010)

---
+++ trunk/ProcessModelShort/src/processModel/Task.java
@@ -0,0 +1,81 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package processModel;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Task</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link processModel.Task#getName <em>Name</em>}</li>
+ *   <li>{@link processModel.Task#getInput <em>Input</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see processModel.ProcessModelPackage#getTask()
+ * @model
+ * @generated
+ */
+public interface Task extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Name</em>' attribute.
+	 * @see #setName(String)
+	 * @see processModel.ProcessModelPackage#getTask_Name()
+	 * @model
+	 * @generated
+	 */
+	String getName();
+
+	/**
+	 * Sets the value of the '{@link processModel.Task#getName <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Name</em>' attribute.
+	 * @see #getName()
+	 * @generated
+	 */
+	void setName(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Input</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Input</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Input</em>' attribute.
+	 * @see #setInput(String)
+	 * @see processModel.ProcessModelPackage#getTask_Input()
+	 * @model
+	 * @generated
+	 */
+	String getInput();
+
+	/**
+	 * Sets the value of the '{@link processModel.Task#getInput <em>Input</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Input</em>' attribute.
+	 * @see #getInput()
+	 * @generated
+	 */
+	void setInput(String value);
+
+} // Task

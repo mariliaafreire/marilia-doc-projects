61cc1045e9b9779fb8fc7f607bdcb0ecdf5086f4
marilia.freire <marilia.freire@gmail.com>
2011-02-10 18:56:24 +0000

    Processo simplificado para testes iniciais da abordagem composta com métricas... (dez 2010)

---
+++ ProcessModelShort/src/processModel/impl/ActivityImpl.java
@@ -0,0 +1,315 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package processModel.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.EObjectResolvingEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import processModel.Activity;
+import processModel.ProcessModelPackage;
+import processModel.Task;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Activity</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link processModel.impl.ActivityImpl#getTask <em>Task</em>}</li>
+ *   <li>{@link processModel.impl.ActivityImpl#getName <em>Name</em>}</li>
+ *   <li>{@link processModel.impl.ActivityImpl#getPredecessor <em>Predecessor</em>}</li>
+ *   <li>{@link processModel.impl.ActivityImpl#getIndex <em>Index</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ActivityImpl extends EObjectImpl implements Activity {
+	/**
+	 * The cached value of the '{@link #getTask() <em>Task</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getTask()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Task> task;
+
+	/**
+	 * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String NAME_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected String name = NAME_EDEFAULT;
+
+	/**
+	 * The cached value of the '{@link #getPredecessor() <em>Predecessor</em>}' reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getPredecessor()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Activity> predecessor;
+
+	/**
+	 * The default value of the '{@link #getIndex() <em>Index</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getIndex()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final int INDEX_EDEFAULT = 0;
+
+	/**
+	 * The cached value of the '{@link #getIndex() <em>Index</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getIndex()
+	 * @generated
+	 * @ordered
+	 */
+	protected int index = INDEX_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ActivityImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ProcessModelPackage.Literals.ACTIVITY;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Task> getTask() {
+		if (task == null) {
+			task = new EObjectContainmentEList<Task>(Task.class, this, ProcessModelPackage.ACTIVITY__TASK);
+		}
+		return task;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setName(String newName) {
+		String oldName = name;
+		name = newName;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ProcessModelPackage.ACTIVITY__NAME, oldName, name));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Activity> getPredecessor() {
+		if (predecessor == null) {
+			predecessor = new EObjectResolvingEList<Activity>(Activity.class, this, ProcessModelPackage.ACTIVITY__PREDECESSOR);
+		}
+		return predecessor;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public int getIndex() {
+		return index;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setIndex(int newIndex) {
+		int oldIndex = index;
+		index = newIndex;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ProcessModelPackage.ACTIVITY__INDEX, oldIndex, index));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case ProcessModelPackage.ACTIVITY__TASK:
+				return ((InternalEList<?>)getTask()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ProcessModelPackage.ACTIVITY__TASK:
+				return getTask();
+			case ProcessModelPackage.ACTIVITY__NAME:
+				return getName();
+			case ProcessModelPackage.ACTIVITY__PREDECESSOR:
+				return getPredecessor();
+			case ProcessModelPackage.ACTIVITY__INDEX:
+				return getIndex();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ProcessModelPackage.ACTIVITY__TASK:
+				getTask().clear();
+				getTask().addAll((Collection<? extends Task>)newValue);
+				return;
+			case ProcessModelPackage.ACTIVITY__NAME:
+				setName((String)newValue);
+				return;
+			case ProcessModelPackage.ACTIVITY__PREDECESSOR:
+				getPredecessor().clear();
+				getPredecessor().addAll((Collection<? extends Activity>)newValue);
+				return;
+			case ProcessModelPackage.ACTIVITY__INDEX:
+				setIndex((Integer)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ProcessModelPackage.ACTIVITY__TASK:
+				getTask().clear();
+				return;
+			case ProcessModelPackage.ACTIVITY__NAME:
+				setName(NAME_EDEFAULT);
+				return;
+			case ProcessModelPackage.ACTIVITY__PREDECESSOR:
+				getPredecessor().clear();
+				return;
+			case ProcessModelPackage.ACTIVITY__INDEX:
+				setIndex(INDEX_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ProcessModelPackage.ACTIVITY__TASK:
+				return task != null && !task.isEmpty();
+			case ProcessModelPackage.ACTIVITY__NAME:
+				return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+			case ProcessModelPackage.ACTIVITY__PREDECESSOR:
+				return predecessor != null && !predecessor.isEmpty();
+			case ProcessModelPackage.ACTIVITY__INDEX:
+				return index != INDEX_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (name: ");
+		result.append(name);
+		result.append(", index: ");
+		result.append(index);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ActivityImpl

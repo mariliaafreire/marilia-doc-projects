05a1e10dec0fab9a9d5dc4974abea5973907e15e
marilia.freire@gmail.com <marilia.freire@gmail.com@fad937e2-7c9b-ea7b-840c-a1296998497c>
2011-02-13 03:16:07 +0000

    Adicção do projeto com os templates Acceleo para o artigo ICEIS2011

---
+++ trunk/ICEISAcceleoProject/results/UCDevActionHandler.java
@@ -0,0 +1,35 @@
+package com.sample.action;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.exe.ExecutionContext;
+
+public class UCDevActionHandler implements ActionHandler {
+
+	private static final long serialVersionUID = 1L;
+	Log log = LogFactory.getLog(this.getClass());
+	 
+	public void execute(ExecutionContext context) throws Exception {
+		long develop = (Long)context.getContextInstance().getVariable("UCDevTime");
+		
+		Date inicio = context.getTaskInstance().getStart();
+		Date fim = context.getTaskInstance().getEnd();
+		
+		long timeTotal = fim.getTime() - inicio.getTime();
+		
+		System.out.println("Time took for  UCDev " + timeTotal + " milliseconds");
+		
+		double hoursTime = timeTotal / (1000.0 * 60.0 * 60.0);
+		
+		log.info(context.getTaskInstance().getName()+ " Updating   UCDev " + hoursTime + "h(s)"));
+		
+		context.getTaskInstance().setVariable("UCDev", hoursTime);
+	}
+	
+	
+
+}
+	

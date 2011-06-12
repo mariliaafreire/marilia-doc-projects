952eb3c0d9354bf9614a17ad5f415ab531a3cbb0
marilia.freire <marilia.freire@fad937e2-7c9b-ea7b-840c-a1296998497c>
2011-06-12 14:11:48 +0000

    Atualização para o trabalho de Metricas. (criação de várias tarefas de um mesmo tipo).

--- trunk/openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/UCReqActionHandler.java
+++ trunk/openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/UCReqActionHandler.java
@@ -1,4 +1,4 @@
-package com.sample.action;
+package br.ufrn.dimap.ppgsc;
 
 import java.util.Date;
 
@@ -7,23 +7,29 @@ import org.apache.commons.logging.LogFactory;
 import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
 
-public class DevelopActionHandler implements ActionHandler {
+public class UCReqActionHandler implements ActionHandler {
 
 	private static final long serialVersionUID = 1L;
 	Log log = LogFactory.getLog(this.getClass());
 	 
 	public void execute(ExecutionContext context) throws Exception {
-		long develop = (Long)context.getContextInstance().getVariable("developTime");
+		double develop = (Double)context.getContextInstance().getVariable("UCReqTime");
 		
 		Date inicio = context.getTaskInstance().getStart();
 		Date fim = context.getTaskInstance().getEnd();
 		
 		long timeTotal = fim.getTime() - inicio.getTime();
 		
-		System.out.println("That took " + timeTotal + " milliseconds");
-		log.info(context.getTaskInstance().getName()+ " Updating conceptionTime"+ timeTotal/ (1000 * 60 * 60));
+		System.out.println("Time took for  UCReq " + timeTotal + " milliseconds");
 		
-		context.getTaskInstance().setVariable("developTime", new Long(timeTotal + develop));
+		double hoursTime = timeTotal / (1000.0 * 60.0 );
+		
+		log.info(context.getTaskInstance().getName()+ " Updating   UCReq " + hoursTime + "h(s)");
+		
+		context.getTaskInstance().setVariable("UCReqTime",Math.round(hoursTime + develop));
 	}
+	
+	
 
 }
+	

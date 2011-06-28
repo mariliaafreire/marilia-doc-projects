1a5a50684a1855170ba685b9f86e3b72df00435a
danielcalencar@gmail.com <danielcalencar@gmail.com@fad937e2-7c9b-ea7b-840c-a1296998497c>
2011-06-28 00:17:01 +0000

    Adicionando ao SVN, as classes referentes ao R

---
+++ branches/OpenUpJPDL_TAESI/openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/jri/CustomGDCanvas.java
@@ -0,0 +1,17 @@
+package br.ufrn.dimap.ppgsc.jri;
+
+import org.rosuda.javaGD.GDObject;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Vector;
+
+import org.rosuda.javaGD.GDCanvas;
+
+public class CustomGDCanvas extends GDCanvas
+{
+	
+   public CustomGDCanvas(double w, double h)
+   {
+      super(w,h);
+   }  
+}
---
+++ branches/OpenUpJPDL_TAESI/openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/jri/CustomJavaGD.java
@@ -0,0 +1,28 @@
+package br.ufrn.dimap.ppgsc.jri;
+
+import javax.swing.JFrame;
+
+import org.rosuda.javaGD.GDInterface;
+
+public class CustomJavaGD extends GDInterface 
+{
+   public static CustomGDCanvas c;
+   JFrame f = new JFrame("JavaGD");
+   
+   //metodo que deve ser sobrescrito para efetuar a exibicao do grafico.
+   public void gdOpen(double w, double h)
+   {
+	  super.c = new CustomGDCanvas(w, h);
+	  f.add((CustomGDCanvas) super.c);
+	  f.pack();
+	  
+	  //atribuindo o canvas para uma referencia estatica, isso facilitara o acesso.
+	  c = (CustomGDCanvas) super.c;
+	  
+	  /* o JFrame nao deve aparecer */
+	  
+	  //f.setVisible(true);
+	 // f.setTitle("Classe customizavel para Plotar");
+	  //f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }    
+}

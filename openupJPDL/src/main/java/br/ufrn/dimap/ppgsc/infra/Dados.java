6f4a1b7deb6efaa84fbad250af3359a853b97064
danielcalencar <danielcalencar@gmail.com>
2011-06-30 00:28:49 +0000

    Dados -> modificando o classpath para achar a classe CustomJavaGD Utils -> adicionando metodos para tentar gerar uma figura atraves de um canvas.

--- openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/infra/Dados.java
+++ openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/infra/Dados.java
@@ -28,21 +28,36 @@ public final class Dados
    {
       Rengine r = RengineFactory.getRengine();
       
+    System.out.println("[Imprimindo o Engine do R]-> " + r.toString());  
+      
     //criando os dados que servirao para calibrar nossa base, depois deve-se mudar para 
     //pegar estes dados de uma base externa, como a aplicacao
     r.eval("dados <- rnorm(400,12,2)");
     r.eval("dadosInteiros <- as.integer(dados)");
     matrizDesenvolvimento = r.eval("matrix(dadosInteiros,ncol=4)");
+    
+    System.out.println("[matrizDesenvolvimento]-> " + matrizDesenvolvimento);
+    
+    long xp_matrizDesenvolvimento = matrizDesenvolvimento.xp;
+    r.rniAssign("matrizDesenvolvimento", xp_matrizDesenvolvimento, 0);
     r.eval("library(qcc)");
-    qccDesenvolvimento = r.eval("qcc(matriz[1:100,],\"xbar\")");
+    qccDesenvolvimento = r.eval("qcc(matrizDesenvolvimento[1:100,],\"xbar\")");
+    
+    System.out.println("[qccDesenvolvimento]-> " + qccDesenvolvimento);
     
     //criando os dados que servirao para calibrar nossa base, depois deve-se mudar para 
     //pegar estes dados de uma base externa, como a aplicacao
     r.eval("dados <- rnorm(400,12,2)");
     r.eval("dadosInteiros <- as.integer(dados)");
     matrizRequisitos = r.eval("matrix(dadosInteiros,ncol=4)");
+    
+    System.out.println("[matrizRequisitos]-> " + matrizRequisitos);
+    
+    long xp_matrizRequisitos = matrizRequisitos.xp;
+    r.rniAssign("matrizRequisitos", xp_matrizRequisitos, 0);
     r.eval("library(qcc)");
-    qccRequisitos = r.eval("qcc(matriz[1:100,],\"xbar\")");
+    qccRequisitos = r.eval("qcc(matrizRequisitos[1:100,],\"xbar\")");
       
+    System.out.println("[qccRequisitos]-> " + qccRequisitos);
    }
 }
--- openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/infra/Utils.java
+++ openupJPDL/src/main/java/br/ufrn/dimap/ppgsc/infra/Utils.java
@@ -2,13 +2,19 @@ package br.ufrn.dimap.ppgsc.infra;
 
 import java.awt.Canvas;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
@@ -19,32 +25,59 @@ public final class Utils
    
    public static void saveCanvasToImageFile(Canvas canvas)
    {
+	   System.out.println("[INICIO CriacaoJPG]");
 	   Frame myFrame = null;
 	   
 	   try
 	   {
-		  myFrame = new Frame();
-		  myFrame.add(canvas);
+		   
+		  BufferedImage image = (BufferedImage) canvas.createImage(canvas.getWidth(), canvas.getHeight()); 
+		  image.getGraphics();
 		  
-	      BufferedImage buffImage = new BufferedImage(canvas.getWidth(),
-			      canvas.getHeight(),BufferedImage.TYPE_INT_RGB);
+		  //myFrame = new Frame();
+		  //myFrame.add(canvas);
+		  
+		  //BufferedImage buffImage = new BufferedImage(canvas.getWidth(),
+		  //      canvas.getHeight(),BufferedImage.TYPE_INT_RGB);
 	      
 	      BufferedOutputStream bos = new BufferedOutputStream
 	              (new FileOutputStream("shewhart.jpg"));
 	   
-	      buffImage.createGraphics().drawImage(buffImage, 0, 0, myFrame);
+	      //buffImage.createGraphics().drawImage(buffImage, 0, 0, myFrame);
 	      JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(bos);
-	      encoder.encode(buffImage);
+	      encoder.encode(image);
 	      
-	      Toolkit tk = Toolkit.getDefaultToolkit();
-	      Image i = tk.getImage("shewhart.jpg");
-	      MediaTracker mt = new MediaTracker( myFrame );
-	      mt.addImage(i, 0);
-	      mt.waitForAll();
+	      
+	      //Toolkit tk = Toolkit.getDefaultToolkit();
+	      //Image i = tk.getImage("shewhart.jpg");
+	      //MediaTracker mt = new MediaTracker( myFrame );
+	      //mt.addImage(i, 0);
+	      //mt.waitForAll();
+	      
+	      //Graphics canGraphic = canvas.getGraphics();
+	      //canGraphic.drawImage(i, 0, 0,myFrame);
+	      //myFrame.repaint();
+	      
+	      //bos.flush();
+	      bos.close();
+	      //buffImage.flush();
 	   }
 	   catch(Exception e)
 	   {
 	      e.printStackTrace();	   
 	   }
+	   System.out.println("[FIM CriacaoJPG]");
    } 
+   
+   public static BufferedImage readImageFromFile(File file) 
+   throws IOException
+   {
+	   return ImageIO.read(file);
+   }
+
+   public static void writeImageToJPG (File file,BufferedImage bufferedImage) 
+   throws IOException
+   {
+	   ImageIO.write(bufferedImage,"jpg",file);
+   }
 }
